14ea6015305853d03fb49bc1174ebec11f9098f5
iyerrag <iyerrag@gmail.com>
2015-04-03 14:55:31 -0400

    added hof

--- MyModule.scala
+++ MyModule.scala
@@ -9,6 +9,16 @@ object MyModule {
     msg.format(x, abs(x))
   }
   
+  private def formatFactorial(n: Int) = {
+    val msg = "The factorial of %d is %d."
+    msg.format(n, factorial(n))
+  }
+  
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+  } 
+  
   def factorial(n:Int): Int = {
     @annotation.tailrec
     def go(n:Int, acc:Int): Int =
@@ -33,5 +43,8 @@ object MyModule {
   def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
     println(fib(4))
+    println(formatFactorial(7))
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial))
   }
 }
